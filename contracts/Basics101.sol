1123b5e18b8673d386ddf339a3d31d224244e3dd
christopher-silvey <webmaster@beckin.com>
2025-05-16 01:04:21 -0500

    Latest

--- contracts/Basics101.sol
+++ contracts/Basics101.sol
@@ -16,5 +16,32 @@ contract Basics101Test {
     uint[5] levels = [0, 1, 2, 3, 4];
 
     // Datetime
-    
+    uint timeNowOneSecond = 1 seconds;
+    uint timeNowOneMinute = 1 minutes;
+    uint timeNowOneHour = 1 hours;
+    uint public timeNowOneDay = 1 days;
+    uint timeNowOneWeek = 1 weeks;
+
+    // Structs
+    struct User {
+        address userAddress;
+        string name;
+        bool hasTraded;
+    }
+
+    // Store a Struct in an Array
+    User[] public users;
+
+    // Mappings
+    mapping (string => string) public accountNameMap;
+
+    // Mapping & Structs Nested
+    mapping (address => mapping (string => User)) private userNestedMap;
+
+    // Enums | Think of them as custom types for things that only have a handful of states.
+    // Under the hood an enum is just a small uint8 (or larger if you have >256 entries),
+    // so storing and comparing them is cheaper than strings or mappings.
+    enum coinRanking {STRONG, CAUTION, DODGY}
+    coinRanking trustLevel;
+    coinRanking public defaultTrustLevel = coinRanking.CAUTION;
 }
